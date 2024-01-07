3951bf835576b05a37010ad96af675b395b01f6d
Krystian Brazulewicz <krystian@brazulewicz.pl>
2024-01-07 22:38:21 +0100

    Simpler identifier in the test

--- squadrats2osm/tests/test_osm.py
+++ squadrats2osm/tests/test_osm.py
@@ -7,17 +7,17 @@ from common.osm import Node
 class TestNode(unittest.TestCase):
     def test_node_to_xml(self):
         """Test building an XML element for simple Node"""
-        node = Node(id = 123, lat = 12.34, lon = 56.78)
+        node = Node(id = -1, lat = 12.34, lon = 56.78)
         elem = node.to_xml()
 
-        self.assertDictEqual(elem.attrib, {'id' : '123', 'lat' : '12.34', 'lon' : '56.78'})
+        self.assertDictEqual(elem.attrib, {'id' : '-1', 'lat' : '12.34', 'lon' : '56.78'})
 
     def test_node_with_tags_to_xml(self):
         """Test building an XML element for Node with tags"""
-        node = Node(id = 123, lat = 12.34, lon = 56.78, tags=[('k1', 'v1'), ('k2', 'v2'), ('k2', 'v2')])
+        node = Node(id = -1, lat = 12.34, lon = 56.78, tags=[('k1', 'v1'), ('k2', 'v2'), ('k2', 'v2')])
         elem = node.to_xml()
 
-        self.assertDictEqual(elem.attrib, {'id' : '123', 'lat' : '12.34', 'lon' : '56.78'})
+        self.assertDictEqual(elem.attrib, {'id' : '-1', 'lat' : '12.34', 'lon' : '56.78'})
         self.assertEqual(len(elem), 3)
         self.assertEqual(elem[0].tag, 'tag')
         self.assertDictEqual(elem[0].attrib, {'k' : 'k1', 'v' : 'v1'})
