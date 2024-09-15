b861fe128ff13cebcde4ed9ca99b24d7dc76e891
Krystian Brazulewicz <kbrazulewicz@atlassian.com>
2024-09-15 08:33:57 +0200

    Move non-parametrized options to the beginning

--- bin/osm2img.sh
+++ bin/osm2img.sh
@@ -26,6 +26,9 @@ typFile="${typFile:-typ/squadrats.typ.txt}"
 # build options for mkgmap
 MKGMAP_OPTS=mkgmap.config
 rm -f $MKGMAP_OPTS
+echo "gmapsupp" >> $MKGMAP_OPTS
+echo "latin1" >> $MKGMAP_OPTS
+echo "transparent" >> $MKGMAP_OPTS
 echo "mapname=${mapName}" >> $MKGMAP_OPTS
 echo "description=${description}" >> $MKGMAP_OPTS
 echo "family-id=${familyId}" >> $MKGMAP_OPTS
@@ -33,7 +36,6 @@ echo "family-name=${familyName}" >> $MKGMAP_OPTS
 echo "product-id=${productId}" >> $MKGMAP_OPTS
 echo "series-name=${seriesName}" >> $MKGMAP_OPTS
 echo "area-name=${areaName}" >> $MKGMAP_OPTS
-echo "transparent" >> $MKGMAP_OPTS
 if [ ! -z "${countryName}" ]; then
     echo "country-name=${countryName}" >> $MKGMAP_OPTS
     echo "country-abbr=${countryAbbr}" >> $MKGMAP_OPTS
@@ -44,13 +46,11 @@ if [ ! -z "${regionName}" ]; then
 fi
 echo "output-dir=${outputDir}" >> $MKGMAP_OPTS
 echo "style-file=${styleFile}" >> $MKGMAP_OPTS
-echo "gmapsupp" >> $MKGMAP_OPTS
 echo "input-file=${typFile}" >> $MKGMAP_OPTS
 echo "input-file=${osmFile}" >> $MKGMAP_OPTS
 
 echo Converting ${osmFile}
 mkdir -p ${outputDir}
-# cat $MKGMAP_OPTS
 mkgmap --read-config=${MKGMAP_OPTS}
 
 if [[ -r "${outputDir}/gmapsupp.img" ]]; then
