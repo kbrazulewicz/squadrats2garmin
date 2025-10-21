38dc6d05b5440e990bdb1233c5037a839e197d93
Krystian Brazulewicz <kbrazulewicz@atlassian.com>
2025-10-21 23:24:58 +0200

    Moved the entire functionality to a python script

--- squadrats2garmin.py
+++ squadrats2garmin.py
@@ -101,8 +101,7 @@ def process_input_job(config_file: str, poly_index: RegionIndex) -> None:
 
     generate_garmin_img(config=config, jobs=jobs)
 
-def remove_all_files(directory_path):
-    directory = pathlib.Path(directory_path)
+def remove_all_files(directory: pathlib.Path):
     for file_path in directory.iterdir():
         if file_path.is_file():
             file_path.unlink()
@@ -127,6 +126,7 @@ if __name__ == "__main__":
 
     # process input jobs
     for config_file in args.config_files:
+        # clear output directory before processing
         remove_all_files(OUTPUT_PATH)
         process_input_job(config_file, poly_index)
         if not args.keep_output:
