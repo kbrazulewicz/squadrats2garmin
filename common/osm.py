894c8bf90a457ca12ef48a16a24c023fec399e6b
Krystian Brazulewicz <kbrazulewicz@atlassian.com>
2025-10-24 19:36:25 +0200

    pylint

--- common/osm.py
+++ common/osm.py
@@ -1,3 +1,5 @@
+"""Classes and methods to work with OSM data
+"""
 import xml.etree.ElementTree as ET
 
 # all squadratinhos
@@ -5,18 +7,22 @@ import xml.etree.ElementTree as ET
 WAY_BASE_ID  = 100000000000
 
 class Tag:
+    """Representation of an OSM tag
+    """
     def __init__(self, k: str, v: str):
         self.k = k
         self.v = v
 
     def to_xml(self):
+        """Generate XML representation of Tag object
+        """
         return ET.Element('tag', {
             'k': self.k,
             'v': str(self.v)
         })
 
 class Node:
-    """Representation of a OSM Node entity
+    """Representation of an OSM Node entity
        See https://wiki.openstreetmap.org/wiki/Node
     """
 
@@ -29,10 +35,12 @@ class Node:
     lon : float = None
     """Node's longitude in degrees"""
 
-    tags : list = None
+    tags : list[Tag] = None
     """Node's tags"""
 
-    def __init__(self, id: int, lat: float, lon: float, tags: list = []) -> None:
+    def __init__(self, id: int, lat: float, lon: float, tags:list[Tag]=None) -> None:
+        if tags is None:
+            tags = []
         self.id  = id
         self.lat = lat
         self.lon = lon
@@ -41,9 +49,11 @@ class Node:
     def __repr__(self) -> str:
         """String representation of the Node object
         """
-        return 'Node(id {}; lat {}; lon {})'.format(self.id, self.lat, self.lon)
+        return f'Node(id={self.id}, lat={self.lat}, lon {self.lon})'
 
     def to_xml(self):
+        """Generate XML representation of Node object
+        """
         node = ET.Element('node', {
             'id': str(self.id),
             'lat': str(self.lat),
@@ -53,20 +63,24 @@ class Node:
             node.append(Tag(k, v).to_xml())
 
         return node
-    
+
 class NodeRef:
+    """Representation of an OSM NodeRef entity
+    """
     ref : int = None
 
     def __init__(self, node: Node) -> None:
         self.ref = node.id
 
     def to_xml(self):
+        """Generate XML representation of NodeRef object
+        """
         return ET.Element('nd', {
             'ref': str(self.ref)
         })
 
 class Way:
-    """Representation of a OSM Way entity
+    """Representation of an OSM Way entity
        See https://wiki.openstreetmap.org/wiki/Way
     """
 
@@ -79,16 +93,20 @@ class Way:
     tags : list[Tag] = None
     """Ways' tags"""
 
-    def __init__(self, id: int, nodes: list[Node], tags: list[Tag] = []) -> None:
+    def __init__(self, id: int, nodes: list[Node], tags: list[Tag]=None) -> None:
+        if tags is None:
+            tags = []
         self.id = id
         self.nodes = nodes
         self.tags = tags
 
     def to_xml(self):
+        """Generate XML representation of Way object
+        """
         way = ET.Element('way', {
             'id': str(self.id)
         })
-        for (node) in self.nodes:
+        for node in self.nodes:
             way.append(NodeRef(node).to_xml())
         for (k, v) in self.tags:
             way.append(Tag(k, v).to_xml())
